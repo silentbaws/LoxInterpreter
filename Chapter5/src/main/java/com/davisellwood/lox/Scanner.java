f09f0c08dceea0379a69d923a5ed20c6fab5bc41
Davis <contact@davisellwood.com>
2024-02-24 18:05:41 -0500

    Finished chapter 5

---
+++ Chapter5/src/main/java/com/davisellwood/lox/Scanner.java
@@ -0,0 +1,217 @@
+package com.davisellwood.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Scanner {
+    private static final Map<String, TokenType> keywords;
+
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    // Challenge 4 code
+    private int commentNestLevel = 0;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",          TokenType.AND);
+        keywords.put("class",        TokenType.CLASS);
+        keywords.put("else",         TokenType.ELSE);
+        keywords.put("false",        TokenType.FALSE);
+        keywords.put("for",          TokenType.FOR);
+        keywords.put("fun",          TokenType.FUN);
+        keywords.put("if",           TokenType.IF);
+        keywords.put("nil",          TokenType.NIL);
+        keywords.put("or",           TokenType.OR);
+        keywords.put("print",        TokenType.PRINT);
+        keywords.put("return",       TokenType.RETURN);
+        keywords.put("super",        TokenType.SUPER);
+        keywords.put("this",         TokenType.THIS);
+        keywords.put("true",         TokenType.TRUE);
+        keywords.put("var",          TokenType.VAR);
+        keywords.put("while",        TokenType.WHILE);
+    }
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while(!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch(c) {
+            case '(': addToken(TokenType.LEFT_PAREN); break;
+            case ')': addToken(TokenType.RIGHT_PAREN); break;
+            case '{': addToken(TokenType.LEFT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            case '.': addToken(TokenType.DOT); break;
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case ';': addToken(TokenType.SEMICOLON); break;
+            case '*': addToken(TokenType.STAR); break;
+            case '!': 
+                addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                break;
+            case '=':
+                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                break;
+            case '>':
+                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                break;
+            case '/':
+                if (match('/')) { // Match for comments
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else if (match('*')) { // Match for multiline comments
+                    // Challenge 4 code
+                    multilineComment();
+                } else {
+                    addToken(TokenType.SLASH);
+                }
+                break;
+            case '"': string(); break;
+            // Skip over blank space and increment line on newline
+            case '\n': line ++;
+            case ' ':
+            case '\r':
+            case '\t':
+            break;
+
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
+                break;
+        }
+    }
+
+    // Challenge 4 code
+    private void multilineComment() {
+        commentNestLevel = 1;
+
+        while (commentNestLevel > 0 && !isAtEnd()) {
+            if (match('/') && peek() == '*') {
+                commentNestLevel++;
+                advance();
+            }
+            if (match('*') && peek() == '/') commentNestLevel--;
+            
+            advance();
+        }
+
+        // TODO: maybe this should log an error if reaching end of file -- user forgot to terminate?
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.getOrDefault(text, TokenType.IDENTIFIER);
+
+        addToken(type);
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for fractions and handle them
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+            
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void string() {
+        // TODO: doesn't appear to support \"
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "unterminated string.");
+            return;
+        }
+
+        advance();
+
+        String value = source.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if(source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <='z') ||
+               (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private char advance() {
+        // This is generally always slower than using a char array
+        // I may do benchmarks later replacing the source definition
+        // TODO: test this
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}
